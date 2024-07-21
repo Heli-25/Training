4d8147bb5ed897b4bd06ee8162fdf3b9ffa3ecfd
Admin <Admin@LAPTOP-9M1M7RDP>
2024-07-21 17:21:00 +0530

    Initial Commit

---
+++ Training/src/package5/Employee.java
@@ -0,0 +1,14 @@
+package package5;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Demo d1 = new Demo();
+	        d1.setId(1); 
+	        d1.setName("John Doe"); 
+	        d1.setDesignation("Manager"); 
+	        System.out.println(d1);
+	}
+
+}
